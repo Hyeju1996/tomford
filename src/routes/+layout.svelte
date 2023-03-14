fc877bed0ec93a958d9100b57c0c24650ba51e03
helena <helena@helenas-MBP.hitronhub.home>
2023-03-14 15:11:09 -0700

    inzilizing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nabla&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<header>
+    <a href="/" class = "site-title">
+        <h1>Tom Ford</h1> 
+        </a>
+        <nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Product</a></li>
+        <li><a href="/">About</a></li>
+    </ul>
+    </nav>
+    <img src="https://thumb.mt.co.kr/06/2023/02/2023021418151336170_1.jpg/dims/optimize/" alt="">
+    <img src="https://thumb.mt.co.kr/06/2023/02/2023021418151336170_2.jpg/dims/optimize/" alt="">
+
+    </header>
+    
